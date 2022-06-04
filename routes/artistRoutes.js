ff643c6d8444e5014b8f536520ff625dcd545e82
adityagoyal222 <goyaladitya85@gmail.com>
2022-06-04 12:49:12 +0545

    fixed case sensitive issue in search api

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -1,22 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Playlist = require("../models/playlistModel");
-const auth = require("../auth/auth");
-const path = require("path");
-const User = require("../models/userModel");
+const Playlist = require('../models/playlistModel');
+const auth = require('../auth/auth');
+const path = require('path');
+const User = require('../models/userModel');
 
-router.get("/artist/search/:searchkey", auth.verifyUser, (req, res) => {
-  const searchkey= req.params.searchkey;
-  User.find({name:{ $regex: '.*' + searchkey + '.*' }}, (err, result) => {
-    if (!err) {
-      console.log(result);
-      return res.status(200).json({ success: true, data: result });
-    } else {
-      return res
-        .status(400)
-        .json({ msg: "Something went wrong.", success: false });
+router.get('/artist/search/:searchkey', auth.verifyUser, (req, res) => {
+  const searchkey = req.params.searchkey;
+  User.find(
+    { name_lower: { $regex: '.*' + searchkey.toLowerCase() + '.*' } },
+    (err, result) => {
+      if (!err) {
+        console.log(result);
+        return res.status(200).json({ success: true, data: result });
+      } else {
+        return res
+          .status(400)
+          .json({ msg: 'Something went wrong.', success: false });
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
