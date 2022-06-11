02254304c7493609309c7b58c1a8eb876f77d585
rdiptan <drt347826@gmail.com>
2022-06-11 19:08:23 +0545

    fix return statement

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -68,10 +68,10 @@ router.put(
         { new: true }
       )
         .then((user) => {
-          res.status(200).json({ user, success: true });
+          return res.status(200).json({ data: user, success: true });
         })
         .catch((err) => {
-          res.status(400).json({ msg: err.message, success: false });
+          return res.status(400).json({ msg: err.message, success: false });
         });
     } else {
       User.findByIdAndUpdate(
@@ -86,10 +86,10 @@ router.put(
         { new: true }
       )
         .then((user) => {
-          res.status(200).json({ user, success: true });
+          return res.status(200).json({ data: user, success: true });
         })
         .catch((err) => {
-          res.status(400).json({ msg: err.message, success: false });
+          return res.status(400).json({ msg: err.message, success: false });
         });
     }
   }
